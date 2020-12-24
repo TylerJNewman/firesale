86b37a54ae2953450925211a0d79b07055cb9813
Tyler Newman <tylerjnewman18@gmail.com>
2020-12-23 17:53:10 -0800

    Wait for load of html before showing window

--- app/main.js
+++ app/main.js
@@ -5,9 +5,11 @@ const { app, BrowserWindow } = require("electron");
 let mainWindow = null;
 
 app.on("ready", () => {
-  mainWindow = new BrowserWindow();
+  mainWindow = new BrowserWindow({ show: false });
 
   mainWindow.loadFile(`${__dirname}/index.html`);
+
+  mainWindow.once("ready-to-show", () => mainWindow.show());
 });
 
 console.log("Starting up...");
